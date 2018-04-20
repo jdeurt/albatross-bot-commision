f1e7bd8b255d4f6cdc94b480c81f9ae21a997158
Sharky! <jdeurt@gmail.com>
2018-04-20 18:37:46 -0500

    Update stop.js

--- stop.js
+++ stop.js
@@ -1,4 +1,4 @@
 module.exports.run = (bot, msg) => {
     if(!msg.member.hasPermission("MANAGE_MESSAGES") || !msg.channel.name.startsWith("private-")) return;
-    msg.channel.delete();
+    msg.channel.delete().catch(console.error);
 };
